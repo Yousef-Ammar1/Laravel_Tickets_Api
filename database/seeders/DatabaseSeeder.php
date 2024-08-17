3a34001771b8a235311b4bf5b7135974e35faa64
JChr15t <yousefaaaammar.com>
2024-08-17 13:26:30 +0300

    Designing the URL

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Ticket;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -13,11 +14,15 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        $users = User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        Ticket::factory(100)->recycle($users)->create();
+        /*
+        In the context of Laravel's factories, the recycle method is used to assign existing models
+        to the factory instead of creating new ones each time.
+        This is particularly useful when you want to associate multiple records with a
+        limited set of existing models.
+        */
+
     }
 }
