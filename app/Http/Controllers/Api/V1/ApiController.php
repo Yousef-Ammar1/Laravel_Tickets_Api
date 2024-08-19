39bda75659e332a585ca4bfb3ab42593a209dfda
JChr15t <yousefaaaammar.com>
2024-08-19 15:39:20 +0300

    Using Policies for user Authorization

--- app/Http/Controllers/Api/V1/ApiController.php
+++ app/Http/Controllers/Api/V1/ApiController.php
@@ -5,10 +5,16 @@ namespace App\Http\Controllers\Api\V1;
 use App\Http\Controllers\Controller;
 use App\Trait\ApiResources;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Gate;
 
 class ApiController extends Controller
 {
     use ApiResources;
+    protected $policyClass;
+
+
+
+
     public function include(string $relationship)
     {
         $param = request()->get('include');
@@ -18,5 +24,15 @@ class ApiController extends Controller
         $includeValues = explode(',', strtolower($param));
 
         return in_array(strtolower($relationship), $includeValues);
+    }
+
+
+    public function isAble($ability, $targetModel)
+    {
+        // This will either allow the action or throw an exception
+        Gate::authorize($ability, $targetModel);
+
+        // If it doesn't throw an exception, return true
+        return true;
     }
 }
